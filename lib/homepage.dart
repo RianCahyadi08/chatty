683eecef28b0d5d89e0c3d85f85d85dcf11e827d
RyanCahyadi <ryancahyadi11@gmail.com>
2023-06-03 23:11:48 +0700

    Add assets images

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -4,11 +4,23 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea(
-      child: Text(
-        'Hello world!',
-        style: TextStyle(fontSize: 40),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Center(
+              child: Image.asset(
+                'assets/images/profile_picture.png',
+                height: 200,
+                width: 200,
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 20.0, horizontal: 0.0),
+              child: Text('Hello world!'),
+            )
+          ],
+        ),
       ),
-    ));
+    );
   }
 }
